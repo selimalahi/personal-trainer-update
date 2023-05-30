1cf5ab33d6b0127476925c7cf0486b5693b0a170
selimalahi <alahi.selim@gmail.com>
2023-05-30 14:30:39 +0600

    last update

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -82,3 +82,140 @@ const Banner = () => {
 };
 
 export default Banner;
+
+
+// import React, { useState } from 'react';
+// import img1 from "../../../assets/banner/banner1.jpg";
+// import img2 from "../../../assets/banner/banner2.jpg";
+// import img3 from "../../../assets/banner/banner3.jpg";
+// import img4 from "../../../assets/banner/banner4.jpg";
+// import img5 from "../../../assets/banner/book-min.png";
+
+// const Banner = () => {
+//   const [activeIndex, setActiveIndex] = useState(0);
+//   const images = [
+//     img1,
+//     img2,
+//     img3,
+//     img4,
+//     img5
+   
+//     // Add more image URLs as needed
+//   ];
+
+//   const handlePrevious = () => {
+//     setActiveIndex((prevIndex) =>
+//       prevIndex === 0 ? images.length - 1 : prevIndex - 1
+//     );
+//   };
+
+//   const handleNext = () => {
+//     setActiveIndex((prevIndex) =>
+//       prevIndex === images.length - 1 ? 0 : prevIndex + 1
+//     );
+//   };
+
+//   return (
+//     <div className="relative">
+//       <div className="flex justify-center items-center">
+//         <button
+//           className="absolute left-0 top-1/2 transform -translate-y-1/2 text-white text-4xl bg-gray-500 hover:bg-gray-600 p-2 rounded-full shadow"
+//           onClick={handlePrevious}
+//         >
+//           &lt;
+//         </button>
+//         <img
+//           className="max-h-96 w-auto"
+//           src={images[activeIndex]}
+//           alt="Carousel Image" 
+//         />
+//         <button
+//           className="absolute right-0 top-1/2 transform -translate-y-1/2 text-white text-4xl bg-gray-500 hover:bg-gray-600 p-2 rounded-full shadow"
+//           onClick={handleNext}
+//         >
+//           &gt;
+//         </button>
+//       </div>
+//       <div className="flex justify-center mt-2">
+//         {images.map((_, index) => (
+//           <div
+//             key={index}
+//             className={`w-4 h-4 mx-2 rounded-full ${
+//               index === activeIndex ? 'bg-gray-800' : 'bg-gray-500'
+//             }`}
+//           ></div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Banner;
+
+// import React, { useState } from 'react';
+// import img1 from "../../../assets/banner/banner1.jpg";
+// import img2 from "../../../assets/banner/banner2.jpg";
+// import img3 from "../../../assets/banner/banner3.jpg";
+// import img4 from "../../../assets/banner/banner4.jpg";
+// import img5 from "../../../assets/banner/book-min.png";
+
+// const Banner = () => {
+//   const [activeIndex, setActiveIndex] = useState(0);
+//   const images = [
+//         img1,
+//         img2,
+//         img3,
+//         img4,
+//         img5
+//     // Add more image URLs as needed
+//   ];
+
+//   const handlePrevious = () => {
+//     setActiveIndex((prevIndex) =>
+//       prevIndex === 0 ? images.length - 1 : prevIndex - 1
+//     );
+//   };
+
+//   const handleNext = () => {
+//     setActiveIndex((prevIndex) =>
+//       prevIndex === images.length - 1 ? 0 : prevIndex + 1
+//     );
+//   };
+
+//   return (
+//     <div className="relative max-w-6xl mx-auto">
+//       <div className="flex justify-center items-center">
+//         <button
+//           className="absolute left-0 top-1/2 transform -translate-y-1/2 text-white text-4xl bg-primary hover:bg-primary-dark p-2 rounded-full shadow transition duration-300"
+//           onClick={handlePrevious}
+//         >
+//           &lt;
+//         </button>
+//         <img
+//           className="w-full w-auto transform transition duration-500 hover:scale-110"
+//           src={images[activeIndex]}
+//           alt="Carousel Image"
+//         />
+//         <button
+//           className="absolute right-0 top-1/2 transform -translate-y-1/2 text-white text-4xl bg-primary hover:bg-primary-dark p-2 rounded-full shadow transition duration-300"
+//           onClick={handleNext}
+//         >
+//           &gt;
+//         </button>
+//       </div>
+//       <div className="flex justify-center mt-2">
+//         {images.map((_, index) => (
+//           <div
+//             key={index}
+//             className={`w-4 h-4 mx-2 rounded-full ${
+//               index === activeIndex ? 'bg-primary' : 'bg-primary-light'
+//             }`}
+//           ></div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Banner;
+
